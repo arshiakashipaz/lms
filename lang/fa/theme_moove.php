76868e914d75f5e482219a09325fd2da290115c6
arshia kashiapaz <murphy.arshia@gmail.com>
2021-01-10 00:11:01 +0330

    :/

---
+++ lang/fa/theme_moove.php
@@ -0,0 +1,281 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package   theme_moove
+ * @copyright 2017 Willian Mano - http://conecti.me
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Moove';
+$string['configtitle'] = 'Moove';
+$string['choosereadme'] = 'Moove is a modern highly-customisable theme. This theme is intended to be used directly, or as a parent theme when creating new themes utilising Bootstrap 4.';
+
+$string['currentinparentheses'] = '(فعلی)';
+$string['region-side-pre'] = 'Right';
+$string['login'] = 'حساب کاربری دارید؟';
+$string['prev_section'] = 'قسمت قبلی';
+$string['next_section'] = 'قسمت بعدی';
+$string['search_site'] = 'جستجو در سایت';
+$string['search_forums'] = 'جستجو در انجمن ها';
+$string['platform_access'] = 'دسترسی به پورتال';
+$string['readmore'] = 'اطلاعات بیشتر';
+$string['stayintouch'] = 'ارتباط با ما';
+$string['madewitmoodle'] = 'با افتخار ساخته شده';
+$string['madeby'] = 'ساخته شده';
+$string['by'] = 'توسط';
+$string['discipline_progress'] = 'Topic\'s progress';
+$string['access'] = 'ورود';
+$string['cachedef_admininfos'] = 'Site administrator dashboard infos cache definition';
+$string['cachedef_fontawesomemooveiconmapping'] = 'Theme icon font definition';
+$string['diskusage'] = 'میزان استفاده از دیسک';
+$string['notcalculatedyet'] = 'محاسبه نشده';
+$string['calculatediskusagetask'] = 'Task to calculate the disk usage';
+$string['totalusers'] = 'کاربران فعال/غیرفعال';
+$string['totalcourses'] = 'تمامی درس ها';
+$string['onlineusers'] = 'کاربران آنلاین ';
+$string['showhideblocks'] = 'نشان/پنهان کردن بلاک ها';
+$string['prev_activity'] = 'فعالیت قبلی';
+$string['next_activity'] = 'فعالیت بعدی';
+
+// General settings tab.
+$string['generalsettings'] = 'تنظیمات اصلی';
+$string['logo'] = 'لوگو';
+$string['logodesc'] = 'لوگو در سربرگ نمایش داده میشود';
+$string['favicon'] = 'نشانک';
+$string['favicondesc'] = 'نشانک خود را آپلود کنید. فایل باید پسوند .ico باشد';
+$string['preset'] = 'قالب تم';
+$string['preset_desc'] = 'با تغییر دادن قالب تم نمای سایت عوض میشود';
+$string['presetfiles'] = 'Additional theme preset files';
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href="https://docs.moodle.org/dev/Boost_Presets">Boost presets</a> for information on creating and sharing your own preset files.';
+$string['loginbgimg'] = 'پس زمینه صفحه ورود';
+$string['loginbgimg_desc'] = 'پس زمینه شخصی سازی شده خود را آپلود کنید';
+$string['brandcolor'] = 'رنگ برند';
+$string['brandcolor_desc'] = 'رنگ برند';
+$string['navbarheadercolor'] = ' رنگ سربرگ نوار ناوبری';
+$string['navbarheadercolor_desc'] = 'رنگ سربرگ نوار ناوبری';
+$string['navbarbg'] = 'رنگ نوار ناوبری';
+$string['navbarbg_desc'] = 'رنگ نوار ناوبری سمت چپ';
+$string['navbarbghover'] = 'رنگ معلق نوار ناوبری';
+$string['navbarbghover_desc'] = 'رنگ نوار ناوبری سمت چپ';
+$string['coursepresentation'] = 'ارائه درس';
+$string['coursepresentationdesc'] = '<b>Default presentation:</b> The default course appearence.<br><b>Cover image:</b> Will be showed a cover image on the top of the course page(the first image in course summary files will be used).';
+$string['coursedefault'] = 'ارائه پیشفرض';
+$string['coursecover'] = 'تصویر جلد';
+$string['courselistview'] = 'فعال سازی نشان دادن لیست درس ها';
+$string['courselistviewdesc'] = 'Switch the courses view from box to list view';
+
+// Advanced settings tab.
+$string['advancedsettings'] = 'پیشرفته';
+$string['rawscsspre'] = 'Raw initial SCSS';
+$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
+$string['rawscss'] = 'Raw SCSS';
+$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
+$string['googleanalytics'] = 'Google Analytics Code';
+$string['googleanalyticsdesc'] = 'Please enter your Google Analytics code to enable analytics on your website. The code format shold be like [UA-XXXXX-Y]';
+
+// Frontpage settings tab.
+$string['frontpagesettings'] = 'صفحه اول';
+$string['disablefrontpageloginbox'] = 'غیرفعال سازی جعبه ورود در صفحه اول';
+$string['disablefrontpageloginboxdesc'] = 'If you check it, the frontpage login box will be hidden and a link to the login page will be printed in the header bar.';
+$string['headerimg'] = 'عکس سربرگ';
+$string['headerimgdesc'] = 'Upload your custom header-image here if you want to add it to the header.mage works best if (Image size should be 1500px x 700px)';
+$string['bannerheading'] = 'سربرگ بنر';
+$string['bannerheadingdesc'] = 'متن سربرگ بنر را وارد کنید';
+$string['bannercontent'] = 'محتوا بنر';
+$string['bannercontentdesc'] = 'متن محتوای بنر را وارد کنید';
+$string['displaymarketingbox'] = 'نشان دادن جعبه مارکتینگ در صفحه اول';
+$string['displaymarketingboxdesc'] = 'Show or Hide Front page Marketingbox';
+$string['marketing1icon'] = 'Marketing1 Icon';
+$string['marketing1icondesc'] = 'Upload Marketing1 Icon';
+$string['marketing1heading'] = 'Marketing1 Heading';
+$string['marketing1headingdesc'] = 'Enter Marketing1 Heading Text';
+$string['marketing1subheading'] = 'Marketing1 Subheading';
+$string['marketing1subheadingdesc'] = 'Enter Marketing1 Subheading Text';
+$string['marketing1content'] = 'Marketing1 Content';
+$string['marketing1contentdesc'] = 'Enter Marketing1 Content Text';
+$string['marketing1url'] = 'Marketing1 URL';
+$string['marketing1urldesc'] = 'Enter Marketing1 URL';
+$string['marketing2icon'] = 'Marketing2 Icon';
+$string['marketing2icondesc'] = 'Upload Marketing2 Icon';
+$string['marketing2heading'] = 'Marketing2 Heading';
+$string['marketing2headingdesc'] = 'Enter Marketing2 Heading Text';
+$string['marketing2subheading'] = 'Marketing2 Subheading';
+$string['marketing2subheadingdesc'] = 'Enter Marketing2 Subheading Text';
+$string['marketing2content'] = 'Marketing2 Content';
+$string['marketing2contentdesc'] = 'Enter Marketing2 Content Text';
+$string['marketing2url'] = 'Marketing2 URL';
+$string['marketing2urldesc'] = 'Enter Marketing2 URL';
+$string['marketing3icon'] = 'Marketing3 Icon';
+$string['marketing3icondesc'] = 'Upload Marketing3 Icon';
+$string['marketing3heading'] = 'Marketing3 Heading';
+$string['marketing3headingdesc'] = 'Enter Marketing3 Heading Text';
+$string['marketing3subheading'] = 'Marketing3 Subheading';
+$string['marketing3subheadingdesc'] = 'Enter Marketing3 Subheading Text';
+$string['marketing3content'] = 'Marketing3 Content';
+$string['marketing3contentdesc'] = 'Enter Marketing3 Content Text';
+$string['marketing3url'] = 'Marketing3 URL';
+$string['marketing3urldesc'] = 'Enter Marketing3 URL';
+$string['marketing4icon'] = 'Marketing4 Icon';
+$string['marketing4icondesc'] = 'Upload Marketing4 Icon';
+$string['marketing4heading'] = 'Marketing4 Heading';
+$string['marketing4headingdesc'] = 'Enter Marketing4 Heading Text';
+$string['marketing4subheading'] = 'Marketing4 Subheading';
+$string['marketing4subheadingdesc'] = 'Enter Marketing4 Subheading Text';
+$string['marketing4content'] = 'Marketing4 Content';
+$string['marketing4contentdesc'] = 'Enter Marketing4 Content Text';
+$string['marketing4url'] = 'Marketing4 URL';
+$string['marketing4urldesc'] = 'Enter Marketing4 URL';
+$string['disableteacherspic'] = 'Disable teachers picture';
+$string['disableteacherspicdesc'] = 'This setting hides the teachers\' pictures from the course cards.';
+
+$string['sliderenabled'] = 'فعال سازی اسلایدر';
+$string['sliderenableddesc'] = 'فعالسازی اسلایدر در صفحه اول شما';
+$string['sliderfrontpage'] = 'نمایش اسلایدر در صفحه اول';
+$string['sliderfrontpagedesc'] = 'If enabled, the slideshow will be showed in the frontpage page replacing the header image.';
+$string['slidercount'] = 'تعداد اسلاید';
+$string['slidercountdesc'] = 'Select how many slides you want to add <strong>then click SAVE</strong> to load the input fields.';
+$string['sliderimage'] = 'عکس اسلاید';
+$string['sliderimagedesc'] = 'Add an image for your slide. Recommended size is 1500px x 540px or higher.';
+$string['slidertitle'] = 'تیتر اسلایدر';
+$string['slidertitledesc'] = 'Add the slide\'s title.';
+$string['slidercaption'] = 'متن اسلایدر';
+$string['slidercaptiondesc'] = 'Add a caption for your slide';
+
+$string['numbersfrontpage'] = 'نمایش اعداد سایت در سفحه اول';
+$string['numbersfrontpagedesc'] = 'If enabled, display the number of active users, courses and activities in the frontpage.';
+$string['frontpagenumbersome'] = 'بعضی';
+$string['frontpagenumbernumbers'] = 'اعداد';
+$string['frontpagenumberusers'] = 'کاربران';
+$string['frontpagenumbercourses'] = 'کلاس ها';
+$string['frontpagenumberactivities'] = 'فعالیت ها';
+
+$string['sponsorstitledefault'] = 'حامیان';
+$string['sponsorssubtitledefault'] = 'شرکت ها و موسس هایی که به ما اعتماد کرده اند';
+$string['sponsorsfrontpage'] = 'فعالسازی نوار حامیان';
+$string['sponsorsfrontpagedesc'] = 'If enabled, display a row with the sponsors logos with its urls.';
+$string['sponsorstitle'] = 'تیتر حامیان';
+$string['sponsorstitledesc'] = 'The header of the sponsors row.';
+$string['sponsorssubtitle'] = 'زیرنویس حامیان';
+$string['sponsorssubtitledesc'] = 'The subtitle of the sponsors row.';
+$string['sponsorscount'] = 'تعداد حامیان';
+$string['sponsorscountdesc'] = 'Select how many sponsor\'s logos you want to add <strong>then click SAVE</strong> to load the input fields.';
+$string['sponsorsimage'] = 'لوگو حامیان';
+$string['sponsorsimagedesc'] = 'Add the sponsor\'s logo.';
+$string['sponsorsurl'] = 'آدرس حامیان';
+$string['sponsorsurldesc'] = 'Enter the sponsor\'s site URL';
+
+$string['clientstitledefault'] = 'مشتری های ما';
+$string['clientssubtitledefault'] = 'مشتری هایی که به ما اعتماد کرده اند';
+$string['clientsfrontpage'] = 'فعالسازی نوار مشتری ها';
+$string['clientsfrontpagedesc'] = 'If enabled, display a row with the clients logos with its urls.';
+$string['clientstitle'] = 'تیتر مشتری ها';
+$string['clientstitledesc'] = 'The header of the clients row.';
+$string['clientssubtitle'] = 'زیر نویس مشتری ها';
+$string['clientssubtitledesc'] = 'The subtitle of the clients row.';
+$string['clientscount'] = 'تعداد مشتری ها';
+$string['clientscountdesc'] = 'Select how many clients\'s logos you want to add <strong>then click SAVE</strong> to load the input fields.';
+$string['clientsimage'] = 'لوگو مشتری ها';
+$string['clientsimagedesc'] = 'Add the client\'s logo.';
+$string['clientsurl'] = 'آدرس مشتری ها';
+$string['clientsurldesc'] = 'Enter the client\'s site URL';
+
+// Footer settings tab.
+$string['footersettings'] = 'پاورقی';
+$string['getintouchcontent'] = 'محتوای تماس با ما';
+$string['getintouchcontentdesc'] = 'محتوای تماس با ما را وارد کنید';
+$string['website'] = 'آدرس سایت';
+$string['websitedesc'] = 'آدرس سایت اصلی';
+$string['mobile'] = 'موبایل';
+$string['mobiledesc'] = 'شماره موبایل را وارد کنید';
+$string['mail'] = 'ایمیل';
+$string['maildesc'] = 'آدرس ایمیل را وارد کنید';
+$string['facebook'] = 'آدرس فیسبوک';
+$string['facebookdesc'] = 'آدرس فیسبوک خود را وارد کنید';
+$string['twitter'] = 'آدرس توییتر';
+$string['twitterdesc'] = 'آدرس توییتر خود را وارد کنید';
+$string['linkedin'] = 'آدرس Linkedin';
+$string['linkedindesc'] = 'آدرس Linkedin خود را وارد کنید';
+$string['youtube'] = 'آدرس یوتیوب';
+$string['youtubedesc'] = 'آدرس یوتیوب خود را وارد کنید';
+$string['instagram'] = 'آدرس اینستاگرام';
+$string['instagramdesc'] = 'آدرس اینستاگرام خود را وارد کنید';
+$string['whatsapp'] = 'شماره واتس آپ';
+$string['whatsappdesc'] = 'شماره واتس آپ خود را وارد کنید';
+$string['topfooterimg'] = 'عکس پاورقی';
+$string['topfooterimgdesc'] = 'Upload your custom footer image here if you want to replace the default image. Recommended size is 1500px x 400px or higher.';
+$string['disablebottomfooter'] = 'غیرفعال سازی پاورقی پایین صفحه';
+$string['disablebottomfooterdesc'] = 'غیر فعالسازی نوار نارنجی پایین صفحه';
+
+// Forum.
+$string['forumsettings'] = 'Forum';
+$string['forumsettingsdesc'] = 'Configure the general forum settings for the theme here.';
+$string['forumhtmlemailheader'] = 'Email header';
+$string['forumhtmlemailheaderdesc'] = 'Configure the email header for the forum messages.';
+$string['forumhtmlemailfooter'] = 'Email footer';
+$string['forumhtmlemailfooterdesc'] = 'Configure the email footer for the forum messages.';
+$string['forumcustomtemplate'] = 'Use a pretty template for forum mails';
+$string['forumcustomtemplatedesc'] = 'Enable that in order to use a pretty template when sending forum messages via email. If not enabled standard formatting of forum mails will be used.';
+
+$string['certificates'] = 'گواهینامه ها';
+$string['certificatestitle'] = 'گواهینامه های من';
+$string['subtitleallcertificates'] = 'گواهینامه ها از تمام درس ها';
+$string['subtitlecoursecertificates'] = 'گواهینامه های درس ها';
+$string['nohavecertificates'] = 'شما هنوز گواهینامه ای دریافت نکرده اید';
+$string['coursesections'] = 'بخش درس ها';
+
+// Mypublic page.
+$string['userprofile'] = 'پروفایل کاربری';
+$string['competencyplans'] = 'برنامه های شایستگی ها';
+$string['details'] = 'جزییات';
+$string['startedon'] = 'آغاز شده در';
+$string['addcontact'] = 'اضافه کردن مخاطب';
+$string['removecontact'] = 'حذف کردن مخاطب';
+$string['certificates'] = 'گواهی ها';
+
+// Theme settings.
+$string['themesettingstitle'] = 'تنظیمات دسترسی ها';
+$string['themesettingsfonttype'] = 'نوع فونت';
+$string['themesettingsdefaultfont'] = 'فونت پیشفرض';
+$string['themesettingsdyslexicfont'] = 'Dyslexic font';
+$string['themesettingsenableaccessibilitytoolbar'] = 'فعالسازی نوار دسترسی ها';
+$string['themesettinggsavedsuccessfully'] = 'تنظیمات دسترسی ها با موفقیت ذخیره شد';
+
+// Accessibility features.
+$string['accessibilityfontsize'] = 'سایز فونت';
+$string['accessibilitydecreasefont'] = 'کاهش سایز فونت';
+$string['accessibilityresetfont'] = 'تنظیم مجدد';
+$string['accessibilityincreasefont'] = 'افزایش سایز فونت';
+$string['accessibilitysitecolor'] = 'رنگ سایت';
+$string['accessibilityresetsitecolor'] = 'تنظیم مجدد رنگ سایت';
+$string['accessibilitysitecolor2'] = 'کنتراست پایین';
+$string['accessibilitysitecolor3'] = 'کنتراست بالا';
+$string['accessibilitysitecolor4'] = 'کنتراست بالا';
+
+// Data privacy.
+$string['privacy:metadata:preference:accessibilitystyles_fontsizeclass'] = 'The user\'s preference for font size.';
+$string['privacy:metadata:preference:accessibilitystyles_sitecolorclass'] = 'The user\'s preference for site color.';
+$string['privacy:metadata:preference:thememoovesettings_fonttype'] = 'The user\'s preference for font type.';
+$string['privacy:metadata:preference:thememoovesettings_enableaccessibilitytoolbar'] = 'The user\'s preference for enable the accessibility toolbar.';
+
+$string['privacy:accessibilitystyles_fontsizeclass'] = 'The current preference for the font size is: {$a}.';
+$string['privacy:accessibilitystyles_sitecolorclass'] = 'The current preference for the site color is: {$a}.';
+$string['privacy:thememoovesettings_fonttype'] = 'The current preference for the font type is: {$a}.';
+$string['privacy:thememoovesettings_enableaccessibilitytoolbar'] = 'The current preference for enable accessibility toolbar is to show it.';
